943779332781de788137cb090e3a97d3768816fb
riand <riand@RICKARDSLAPTOP>
2022-10-25 15:18:03 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClassListSort
+{
+    public class Book
+    {
+        public string? Title { get; set; }
+        public int Year { get; set; }
+        public int Pages { get; set; }
+        public override string ToString()
+        {
+            return string.Format("[{0}] {1} {2}", this.Title, this.Pages, this.Year);
+
+        }
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+
+namespace BookClassListSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new List<Book>();
+
+            Console.WriteLine("Vad heter boken?");
+            string svar1 = Console.ReadLine();
+            Console.WriteLine("Hur många sidor innehåller boken?");
+            int svar2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("När släpptes boken?");
+            int svar3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv in en ytterliggare en bok");
+
+            Console.WriteLine("Vad heter boken?");
+            string svar4 = Console.ReadLine();
+            Console.WriteLine("Hur många sidor innehåller boken?");
+            int svar5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("När släpptes boken?");
+            int svar6 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv in ytterligare en bok");
+
+            Console.WriteLine("Vad heter boken?");
+            string svar7 = Console.ReadLine();
+            Console.WriteLine("Hur många sidor innehåller boken?");
+            int svar8 = int.Parse(Console.ReadLine());
+            Console.WriteLine("När släpptes boken?");
+            int svar9 = int.Parse(Console.ReadLine());
+
+            books.Add(new Book { Title = svar1, Pages = svar2, Year = svar3 });
+            books.Add(new Book { Title = svar4, Pages = svar5, Year = svar6 });
+            books.Add(new Book { Title = svar7, Pages = svar8, Year = svar9 });
+
+            books.Sort((b1, b2) => b1.Year.CompareTo(b2.Year));
+            books.Reverse();
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+}   }
